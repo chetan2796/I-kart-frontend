78ab6ec71b0f80d6b0f5ee11e419795ce328c06d
Nainshree Verma <vnainshri1505@gmail.com>
2025-06-25 19:42:43 +0530

    Implemented SignOut API

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,6 +1,35 @@
 import Link from "next/link";
-
+import { useRouter } from "next/navigation";
 const Header = () => {
+  const router = useRouter();
+  const handleSignout = async () => {
+    const email = localStorage.getItem("email");
+
+    if (!email) {
+      alert("No user is logged in.");
+      return;
+    }
+
+    try {
+      const response = await fetch("http://localhost:3000/auth/signout", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email }),
+      });
+      if (!response.ok) {
+        throw new Error("Failed to sign out");
+      }
+      localStorage.removeItem("email");
+      localStorage.removeItem("isLoggedIn");
+      router.push("user/login");
+    } catch (error) {
+      console.error("Error during signout:", error);
+      alert("Signout failed. Please try again.");
+    }
+  };
+
   return (
     <header className="header">
       <nav
@@ -37,12 +66,12 @@ const Header = () => {
               </Link>
             </li>
             <li>
-              <Link
-                href="/users_sign_out_path"
+              <button
+                onClick={handleSignout}
                 className="btn btn-outline-primary btn-sm me-2 dropdown-item text-black"
               >
                 Sign Out
-              </Link>
+              </button>
             </li>
           </ul>
         </div>
