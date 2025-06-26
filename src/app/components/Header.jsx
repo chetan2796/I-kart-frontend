d42008220b9934a1c353feca87d664e49d41dad8
Chetan <chetan.codenote@gmail.com>
2025-06-26 17:46:39 +0530

    [main] remove header form i-kart

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -34,42 +34,6 @@ const Header = () => {
 
   return (
     <header className="header">
-      <nav
-        className="navbar navbar-expand-lg navbar-light bg-white shadow-sm flex p-5"
-      >
-        <div className="container">
-          <Link
-            href="/dashboardSeller"
-            className="btn btn-outline-primary btn-sm me-2 navbar-bra nd fw-bold text-black"
-          >
-            i-kart
-          </Link>
-        </div>
-
-        <div className="d-flex align-items-center ms-auto">
-          <ul
-            className="dropdown-menu dropdown-menu-end flex"
-            aria-labelledby="userDropdown"
-          >
-            <li>
-              <Link
-                href="/profile_path"
-                className="btn btn-outline-primary btn-sm me-2 dropdown-item text-black"
-              >
-                Profile
-              </Link>
-            </li>
-            <li>
-              <button
-                onClick={handleSignout}
-                className="btn btn-outline-primary btn-sm me-2 dropdown-item text-black"
-              >
-                Sign Out
-              </button>
-            </li>
-          </ul>
-        </div>
-      </nav>
     </header>
   );
 };
