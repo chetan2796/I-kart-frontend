b4d715942690056ed443a4ddf813b61cda9e433e
Geetanjali <geetanjali.codenote@gmail.com>
2025-07-02 11:26:18 +0530

    Fixed link tag of sidebar

--- src/app/components/Sidebar.jsx
+++ src/app/components/Sidebar.jsx
@@ -10,9 +10,9 @@ export default function Sidebar() {
     <div className="w-64 min-h-screen p-6 bg-gray-100">
       <h2 className="text-2xl font-bold mb-6">I-Kart</h2>
       <ul className="space-y-4">
-        <li className='hover:bg-gray-200 p-2 cursor-pointer'><Link href="/dashboardSeller">Dashboard</Link></li>
-        <li className='hover:bg-gray-200 p-2 cursor-pointer'><Link href="/catalogs">Catalogs</Link></li>
-        <li className='hover:bg-gray-200 p-2 cursor-pointer'><Link href="/dashboardStore/store">My Stores</Link></li>
+        <Link href="/dashboardSeller"><li className='hover:bg-gray-200 p-2 cursor-pointer'>Dashboard</li></Link>
+        <Link href="/catalogs"><li className='hover:bg-gray-200 p-2 cursor-pointer'>Catalogs</li></Link>
+        <Link href="/dashboardStore/store"><li className='hover:bg-gray-200 p-2 cursor-pointer'>My Stores</li></Link>
         <li
           className='hover:bg-gray-200 p-2 cursor-pointer'
           onClick={()=>handleSignout(router)}
